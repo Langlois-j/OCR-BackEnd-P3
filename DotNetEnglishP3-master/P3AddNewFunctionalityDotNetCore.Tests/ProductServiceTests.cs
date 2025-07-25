7ee31e552c11f1ef1ce4fc995e67fadcec1696a9
Langlois-j <langlois.julien37@gmail.com>
2025-07-25 10:03:52 +0200

    Initiation test et correction Langue Espagnol

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,7 +1,11 @@
-﻿using P3AddNewFunctionalityDotNetCore.Models;
+﻿using Microsoft.CodeAnalysis;
+using P3AddNewFunctionalityDotNetCore.Models;
 using P3AddNewFunctionalityDotNetCore.Models.Entities;
 using P3AddNewFunctionalityDotNetCore.Models.Repositories;
 using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System.Collections.Generic;
+using Xunit;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
@@ -12,6 +16,21 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         /// A test method must check if a definite method does its job:
         /// returns an expected value from a particular set of parameters
         /// </summary>
+        ProductService LocalProductService = new ProductService(ProductServiceTests.ProductRepositoryMock.Object,
+            ProductServiceTests.OrderRepositoryMock.Object, ProductServiceTests.CartMock.Object,
+            ProductServiceTests.LocalizerMock.Object);  
+
+        ProductViewModel TestProduct= new ProductViewModel
+        {
+            Id = 1,
+            Name = "Test Product",
+            Price = "10.0",
+            Stock = "100",
+            Description = "This is a test product",
+            Details = "Test details"
+        };
+        private object _localizer;
+
         [Fact]
         public void ExampleMethod()
         {
@@ -25,5 +44,44 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
             Assert.Equal(1, 1);
         }
         // TODO write test methods to ensure a correct coverage of all possibilities
+        [Fact]
+        public void GetOneProduct_ReturnProduct()
+        {
+            //// Arrange
+            //const int productId = 1;
+
+            //// Act
+            //Product Result=Productservice.GetProductById(productId);
+
+            //// Assert
+            //Assert.Equal(productId, actual: Result.Id);
+            Assert.Equal(1, 1);
+        }
+        [Fact]
+        public void CkeckProduct_ReturnMissingName()
+        {
+            // Arrange
+
+            ProductViewModel LocalProduct = TestProduct;
+                LocalProduct.Name = "";
+
+            // Act
+            List<string> errors = Productservice.CheckProductModelErrors(LocalProduct);
+
+            // Assert
+            // Assert.Equal(errors[0], _localizer["MissingName"]);
+            Assert.Equal(1, 1);
+        }
+        public void CkeckProduct_ReturnMissingPrice()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+            Assert.Equal(1, 1);
+        }
     }
 }
