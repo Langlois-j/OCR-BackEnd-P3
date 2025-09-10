359604b64d5987bb89e5ee2edf6bd38cfb320c1c
Langlois-j <langlois.julien37@gmail.com>
2025-09-10 11:59:22 +0200

    Maj memorytest

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceInMemoryTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceInMemoryTests.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Linq;
 using Xunit;
 using P3AddNewFunctionalityDotNetCore.Models;
 using P3AddNewFunctionalityDotNetCore.Models.Services;
 using P3AddNewFunctionalityDotNetCore.Models.Repositories;
 using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
-
+using P3AddNewFunctionalityDotNetCore.Models.Entities;
+using P3AddNewFunctionalityDotNetCore.Data;
 
 
 
@@ -22,6 +22,13 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         //        .Options;
         //    return new ApplicationDbContext(options);
         //}
+        private P3Referential GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<P3Referential>()
+                .UseInMemoryDatabase(databaseName: System.Guid.NewGuid().ToString()) // DB unique par test
+                .Options;
+            return new P3Referential(options, null);
+        }
 
         private ProductViewModel GetTestProductViewModel()
         {
@@ -51,11 +58,60 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
             service.SaveProduct(testProduct);
 
             // Assert
-            var product = context.Products.FirstOrDefault(p => p.Name == "Produit Test");
+            var product = service.GetProduct(testProduct.Id);
             Assert.NotNull(product);
             Assert.Equal(10, product.Quantity);
             Assert.Equal(20, product.Price);
+            //netoyage de la base
             service.DeleteProduct(testProduct.Id);
         }
+        [Fact]
+        public void ProductDeleteData()
+        {
+            // Arrange
+            using var context = GetInMemoryDbContext();
+            var repo = new ProductRepository(context);
+            var orderRepo = new OrderRepository(context);
+            var cart = new Cart();
+            var service = new ProductService(cart, repo, orderRepo, null);
+            var testProduct = GetTestProductViewModel();
+
+            //ajoute du produit a supprimer
+            service.SaveProduct(testProduct);
+            // Act
+
+            service.DeleteProduct(testProduct.Id);
+            // Assert
+            var product = context.Products.FirstOrDefault(p => p.Name == "Produit Test");
+            Assert.Null(product);
+          
+        }
+                [Fact]
+        public void ProductUpdateQuantitiesData()
+        {
+            // Arrange
+            using var context = GetInMemoryDbContext();
+            var repo = new ProductRepository(context);
+            var orderRepo = new OrderRepository(context);
+            var cart = new Cart();
+            var service = new ProductService(cart, repo, orderRepo, null);
+            var testProduct = GetTestProductViewModel();
+
+            //ajoute du produit a Modifier
+            service.SaveProduct(testProduct);
+            //modifiaction des quantitée
+            testProduct.Stock = "1";
+
+            // Act
+
+            service.UpdateProductQuantities(testProduct);
+            // Assert
+            var product = context.Products.FirstOrDefault(p => p.Name == "Produit Test");
+            Assert.Null(product);
+          
+            //netoyage de la base
+            service.DeleteProduct(testProduct.Id);
+        }
+
     }
 }
